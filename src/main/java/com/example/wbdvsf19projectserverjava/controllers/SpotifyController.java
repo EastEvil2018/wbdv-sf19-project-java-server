14b654adeb81a870b94b76ccf00a4512d73b9aaa
EastEvil2018 <eastevil95@gmail.com>
2019-07-15 16:22:16 -0400

    Test for getAccessToken

---
+++ src/main/java/com/example/wbdvsf19projectserverjava/controllers/SpotifyController.java
@@ -0,0 +1,28 @@
+package com.example.wbdvsf19projectserverjava.controllers;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.wbdvsf19projectserverjava.services.SpotifyService;
+
+
+@RestController
+@CrossOrigin(origins="*", maxAge=3600)
+public class SpotifyController {
+	
+	@Autowired
+	SpotifyService spotifyService;
+	
+	@PostMapping("/api/token/{clientId}")
+	public Object requestAccessToken
+	(@PathVariable("clientId") String clientId,
+	 @RequestBody String clientSecret) throws IOException {
+		return spotifyService.requestAccessToken(clientId, clientSecret);
+	}
+}
---
+++ src/main/java/com/example/wbdvsf19projectserverjava/services/SpotifyService.java
@@ -0,0 +1,60 @@
+package com.example.wbdvsf19projectserverjava.services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpotifyService {
+	
+	public Object requestAccessToken(String clientId, String clientSecret) throws IOException {
+	    String POST_PARAMS = "grant_type=client_credentials";
+	    String userCredentials = clientId + ":" + clientSecret;
+	    String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
+	    URL obj = new URL("https://accounts.spotify.com/api/token");
+	    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	    con.setRequestMethod("POST");
+	            con.setRequestProperty("Content-Type", "application/json;odata=verbose");
+	    con.setRequestProperty("Authorization",
+	            "Basic " + basicAuth);
+	    con.setRequestProperty("Accept",
+	            "application/x-www-form-urlencoded");
+
+	    // For POST only - START
+	    con.setDoOutput(true);
+	    OutputStream os = con.getOutputStream();
+	    os.write(POST_PARAMS.getBytes());
+	    os.flush();
+	    os.close();
+	    // For POST only - END
+
+	    int responseCode = con.getResponseCode();
+	    System.out.println("POST Response Code :: " + responseCode);
+
+	    if (responseCode == HttpURLConnection.HTTP_OK) { //success
+	        BufferedReader in = new BufferedReader(new InputStreamReader(
+	                con.getInputStream()));
+	        String inputLine;
+	        StringBuffer response = new StringBuffer();
+
+	        while ((inputLine = in.readLine()) != null) {
+	            response.append(inputLine);
+	        }
+	        in.close();
+
+	        // print result
+	        System.out.println(response.toString());
+	        return response;
+	    } else {
+	        System.out.println("POST request not worked");
+	        return responseCode;
+	    }
+	}
+}
