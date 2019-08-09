7457c234f1531bf4761e5a1bd8631c4fe4f0f6c5
yidibj <yi.di@husky.neu.edu>
2019-08-09 17:02:21 -0400

    updates user module

--- src/main/java/com/example/wbdvsf19projectserverjava/controllers/UserController.java
+++ src/main/java/com/example/wbdvsf19projectserverjava/controllers/UserController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="*", maxAge=3600)
+@CrossOrigin(origins="*", maxAge=3600, allowCredentials = "true")
 public class UserController {
 	@Autowired
 	UserRepository userRepository;	
